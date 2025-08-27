d59c81f655a2e21d874ded366f00af59a5802e5e
shuy4m4 <shuya.kamiyama@gmail.com>
2025-08-27 10:58:46 +0900

    update server.js

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.post('/webhook', express.raw({ type: 'application/json' }), async (req, res)
 
   if (event.type === 'checkout.session.completed') {
     try {
-      const response = await fetch('https://script.google.com/macros/s/AKfycbzRPtnAssb5eZZ8lhFAqLDqEWfrrUdgiGdfc_8zUzSZ_yFrbF6wxd7CigF_bljV_R0h/exec', {
+      const response = await fetch('https://script.google.com/macros/s/AKfycbzMyJQ52kummd889p1-1kASbt-ixpzLzzcm7JwXSGC0JtY_wIUFezXCGWWqXAmF1Uz2/exec', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(event) // ✅ event 全体を送る
